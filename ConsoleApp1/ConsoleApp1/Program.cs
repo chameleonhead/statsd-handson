3acad89f926357583e94f8254a5665deaae48805
Yuto Nagano <chamelion@hotmail.co.jp>
2021-08-17 12:49:33 +0900

    statsd set up.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using StatsN;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var statsd = Statsd.New<Udp>(a => { 
+                a.HostOrIp = "localhost"; 
+                a.Port = 8125;
+                a.Prefix = "MyMicroserviceName"; 
+            });
+            statsd.CountAsync("myapp.counterstat");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
