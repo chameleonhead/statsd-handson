d9d473be652b4996f7520f0af1e56ea2876f3ef5
Yuto Nagano <chamelion@hotmail.co.jp>
2021-08-17 14:04:33 +0900

    change program to send multiple counterstats.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,19 +1,28 @@
 ﻿using StatsN;
 using System;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => MainAsync(args).Wait();
+
+        static async Task MainAsync(string[] args)
         {
-            var statsd = Statsd.New<Udp>(a => { 
-                a.HostOrIp = "localhost"; 
+            var statsd = Statsd.New<Udp>(a =>
+            {
+                a.HostOrIp = "localhost";
                 a.Port = 8125;
-                a.Prefix = "MyMicroserviceName"; 
+                a.Prefix = "MyMicroserviceName";
             });
-            statsd.CountAsync("myapp.counterstat");
+
             Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.ReadKey();
+                await statsd.CountAsync("myapp.counterstat");
+            }
         }
     }
 }
