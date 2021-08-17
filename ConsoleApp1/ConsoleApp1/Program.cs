d31283ed8ad00ac71c22dd4dc9f2ec5870ba4e4b
Yuto Nagano <chamelion@hotmail.co.jp>
2021-08-17 12:49:33 +0900

    statsd set up.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using Prometheus;
+using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static readonly Counter TickTock =
+            Metrics.CreateCounter("sampleapp_ticks_total", "Just keeps on ticking");
+
+        static void Main()
+        {
+            var server = new MetricServer(hostname: "localhost", port: 1234);
+            server.Start();
+
+            while (true)
+            {
+                TickTock.Inc();
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
