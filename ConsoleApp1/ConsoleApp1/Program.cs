b88b1f1d240072ad0ca82496e23c7c4136c34b4d
Yuto Nagano <chamelion@hotmail.co.jp>
2021-08-21 17:20:20 +0900

    add zipkin

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,8 @@
-﻿using Prometheus;
+﻿using OpenTelemetry;
+using OpenTelemetry.Trace;
+using Prometheus;
 using System;
+using System.Diagnostics;
 using System.Threading;
 
 namespace ConsoleApp1
@@ -9,13 +12,30 @@ namespace ConsoleApp1
         private static readonly Counter TickTock =
             Metrics.CreateCounter("sampleapp_ticks_total", "Just keeps on ticking");
 
+        private static readonly ActivitySource MyActivitySource = new ActivitySource(
+            "MyCompany.MyProduct.MyLibrary");
+
         static void Main()
         {
+            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .SetSampler(new AlwaysOnSampler())
+                .AddSource("MyCompany.MyProduct.MyLibrary")
+                .AddZipkinExporter(options => { options.Endpoint = new Uri("http://zipkin:9411/api/v2/spans"); })
+                .Build();
+
             var server = new MetricServer(hostname: "*", port: 1234);
             server.Start();
 
             while (true)
             {
+
+                using (var activity = MyActivitySource.StartActivity("SayHello"))
+                {
+                    activity?.SetTag("foo", 1);
+                    activity?.SetTag("bar", "Hello, World!");
+                    activity?.SetTag("baz", new int[] { 1, 2, 3 });
+                }
+
                 TickTock.Inc();
                 Thread.Sleep(TimeSpan.FromSeconds(1));
             }
